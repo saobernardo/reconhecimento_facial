b76a4b8f5d78b002653ad3856f04df2dc8c19e88
Lucas São Bernardo <bernardo.pinheiro@hotmail.com>
2024-10-06 15:51:16 -0300

    Finalizado marcação facial em fotos

--- extract_faces.py
+++ extract_faces.py
@@ -20,16 +20,34 @@ img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
 detector = MTCNN()
 result = detector.detect_faces(img)
 
+print(result)
+
 x1, y1, width, height = result[0]['box']
+left_eye, right_eye = result[0]['keypoints']['left_eye'], result[0]['keypoints']['right_eye']
+mouth_left, mouth_right = result[0]['keypoints']['mouth_left'], result[0]['keypoints']['mouth_right']
+nose = result[0]['keypoints']['nose']
 
 fig = plt.figure()
+
+radius_size = 6
+
+supposed_height_value = 50
+count = 1
+while supposed_height_value < height:
+  radius_size = radius_size + 1
+  supposed_height_value = supposed_height_value * count
+  count = count + 1
+
 ax = fig.add_subplot(111)
 ax.imshow(img)
 ax.add_patch(Rectangle((x1,y1), width, height, fill=False, ec='r', lw=2))
+ax.add_patch(Circle(left_eye, fill=True, color='red', radius=radius_size))
+ax.add_patch(Circle(right_eye, fill=True, color='red', radius=radius_size))
+ax.add_patch(Circle(mouth_left, fill=True, color='red', radius=radius_size))
+ax.add_patch(Circle(mouth_right, fill=True, color='red', radius=radius_size))
+ax.add_patch(Circle(nose, fill=True, color='red', radius=radius_size))
 
 ax.axis('off')
-plt.savefig(parametros[0]+'''_marked.png''', pad_inches=0.1)
-
-
+#plt.savefig(parametros[0]+'''_marked.png''', pad_inches=0.1)
 
 plt.show()
